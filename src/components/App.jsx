5bf4a731374513d354be7cc59a3d3f8b5e82e9d8
AlexanderDmitriev <alex9110077@gmail.com>
2022-05-02 02:27:25 +0300

    Global style
    Проведена нормализация стилей

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { ToastContainer } from 'react-toastify';
+import GlobalStyle from '../GlobalStyle';
 import {MovieDataPage} from '../pages/MovieDataPage'
 
 export const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <MovieDataPage/>
       <ToastContainer autoClose={2000} />
     </div>
