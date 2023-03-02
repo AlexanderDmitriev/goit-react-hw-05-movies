e4b89442901b6a672697f403732ceeecca70a9cf
AlexanderDmitriev <alex9110077@gmail.com>
2023-03-02 01:15:17 +0100

    1

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './components/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-   <React.StrictMode> 
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter basename="/goit-react-hw-05-movies/">
         <App />
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode> 
-
+  </React.StrictMode>
 );
